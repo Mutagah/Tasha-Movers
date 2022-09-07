7d59018fab741cea68577a44fbe54da4351fc7b3
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-08 02:36:36 +0300

    styled and added data to the register house component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, "application/json"
-  #   specialization :allow_methods, "GET, HEAD, POST, PUT, OPTIONS, DELETE"
-  #   set :allow_origin, "http:/localhost:3000"
-  #    set :bind, '0.0.0.0'
-  # configure do
-  #   enable :cross_origin
-  # end
-  # before do
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  # end
+    # specialization :allow_methods, "GET, HEAD, POST, PUT, OPTIONS, DELETE"
+    set :allow_origin, "http:/localhost:3000"
+     set :bind, '0.0.0.0'
+  configure do
+    enable :cross_origin
+  end
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
   
   # # routes...
   # options "*" do
